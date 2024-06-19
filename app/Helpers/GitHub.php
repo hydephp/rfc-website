ce177a5e03de424bbf4e5946aef72bc3ede748af
Caen De Silva <caen@desilva.se>
2024-06-19 22:00:37 +0200

    Set the GitHub API version header

--- app/Helpers/GitHub.php
+++ app/Helpers/GitHub.php
@@ -28,6 +28,7 @@ class GitHub
             return Http::withToken(env('GITHUB_TOKEN'))
                 ->withHeaders([
                     'Accept' => 'application/vnd.github.v3+json',
+                    'X-GitHub-Api-Version' => '2022-11-28',
                 ])
                 ->$method('https://api.github.com/repos/'.self::REPOSITORY."/$uri", $data)
                 ->throw()->json();
