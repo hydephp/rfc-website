4067a1ae1f0b9a2526e655bf3f520873ce954d96
Caen De Silva <caen@desilva.se>
2024-06-19 19:47:18 +0200

    Document cases

--- app/Types/Status.php
+++ app/Types/Status.php
@@ -7,8 +7,23 @@ namespace App\Types;
  */
 enum Status
 {
+    /**
+     * The RFC is still in the draft stage. Only applies to PR RFCs.
+     */
     case Draft;
+
+    /**
+     * The RFC has been implemented. (The PR has been merged, or the issue/discussion has been closed with a resolution.)
+     */
     case Implemented;
+
+    /**
+     * The RFC has been rejected. (The PR has been closed without merging, or the issue/discussion has been closed without resolution.)
+     */
     case Rejected;
+
+    /**
+     * The RFC is held back for further discussion. (No activity has been seen for a while.)
+     */
     case Stale;
 }
