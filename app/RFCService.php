f8d4ceb02e50270e6382ddb29e547d05d23d158c
Caen De Silva <caen@desilva.se>
2024-06-19 21:46:34 +0200

    Get issues and pull requests

--- app/RFCService.php
+++ app/RFCService.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Helpers\GitHub;
+
 class RFCService
 {
     public function handle(): void
     {
-        //
+        $issues = GitHub::request('get', 'issues', ['labels' => 'RFC']);
+        $pulls = GitHub::request('get', 'pulls', ['labels' => 'RFC']);
     }
 }
