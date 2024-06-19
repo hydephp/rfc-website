c1e41a39921ba7f223d71f9cc705e9c50e98aead
Caen De Silva <caen@desilva.se>
2024-06-19 20:12:06 +0200

    Generate constructor

--- app/Types/IssueList.php
+++ app/Types/IssueList.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace App\Types;
 
+use App\Helpers\Generics;
+
 class IssueList
 {
     /**
      * @var array<array-key, Issue>
      */
     protected array $issues;
+
+    /**
+     * Create a new IssueList object from an array of Issue objects.
+     *
+     * @param array<array-key, Issue> $issues
+     */
+    public function __construct(array $issues)
+    {
+        $this->issues = Generics::typeSafeArray($issues, Issue::class);
+    }
 }
