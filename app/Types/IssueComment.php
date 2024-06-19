f06acbf261e804f7f46680fca8541803a5ea5dd7
Caen De Silva <caen@desilva.se>
2024-06-19 20:09:57 +0200

    Use typed author object

--- app/Types/IssueComment.php
+++ app/Types/IssueComment.php
@@ -8,13 +8,13 @@ use DateTimeImmutable;
 
 readonly class IssueComment
 {
-    public string $author;
+    public GitHubUser $author;
     public string $body;
 
     public DateTimeImmutable $createdAt;
     public DateTimeImmutable $updatedAt;
 
-    public function __construct(string $author, string $body, DateTimeImmutable $createdAt, DateTimeImmutable $updatedAt) {
+    public function __construct(GitHubUser $author, string $body, DateTimeImmutable $createdAt, DateTimeImmutable $updatedAt) {
         $this->author = $author;
         $this->body = $body;
         $this->createdAt = $createdAt;
