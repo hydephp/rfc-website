988f9337d3062d1b579075c2017d135d8dd8e5b5
Caen De Silva <caen@desilva.se>
2024-06-19 21:55:50 +0200

    Map issues to the typed object

--- app/RFCService.php
+++ app/RFCService.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Types\Issue;
+use App\Types\Status;
+use DateTimeImmutable;
 use App\Helpers\GitHub;
+use App\Types\IssueType;
+use App\Types\GitHubUser;
+use Illuminate\Support\Arr;
+use Hyde\Markdown\Models\Markdown;
 
 class RFCService
 {
@@ -12,5 +19,19 @@ class RFCService
     {
         $issues = GitHub::request('get', 'issues', ['labels' => 'RFC']);
         $pulls = GitHub::request('get', 'pulls', ['labels' => 'RFC']);
+
+        $issues = Arr::map($issues, function (array $issue): Issue {
+            return new Issue(
+                $issue['number'],
+                $issue['title'],
+                new Markdown($issue['body']),
+                new GitHubUser($issue['user']['login']),
+                IssueType::Issue,
+                Status::Draft, // Todo: Determine the status
+                [], // Todo: Get the comments
+                new DateTimeImmutable($issue['created_at']),
+                new DateTimeImmutable($issue['updated_at']),
+            );
+        });
     }
 }
