a357e4566aa5d96f2da75fa98a537e26065f6916
Caen De Silva <caen@desilva.se>
2024-06-19 19:26:16 +0200

    Replace qualifier with an import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\CallRFCServiceCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->commands([
-            \App\CallRFCServiceCommand::class,
+            CallRFCServiceCommand::class,
         ]);
     }
 
