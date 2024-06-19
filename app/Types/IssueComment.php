64dfdd237d8db1176d0ffc0ec3d2bad982ee53cd
Caen De Silva <caen@desilva.se>
2024-06-19 19:55:23 +0200

    Replace qualifiers with an import

--- app/Types/IssueComment.php
+++ app/Types/IssueComment.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Types;
 
+use DateTimeImmutable;
+
 readonly class IssueComment
 {
     public string $author;
     public string $body;
 
-    public \DateTimeImmutable $createdAt;
-    public \DateTimeImmutable $updatedAt;
+    public DateTimeImmutable $createdAt;
+    public DateTimeImmutable $updatedAt;
 }
