cd8b443322f868beb0e8488bac8789954c7690b4
Caen De Silva <caen@desilva.se>
2024-06-19 20:09:25 +0200

    Add issue author property

--- app/Types/Issue.php
+++ app/Types/Issue.php
@@ -11,6 +11,7 @@ readonly class Issue
     public int $number;
     public string $title;
     public string $body;
+    public GitHubUser $author;
     public IssueType $type;
     public Status $status;
 
@@ -19,11 +20,12 @@ readonly class Issue
      */
     public array $comments;
 
-    public function __construct(int $number, string $title, string $body, IssueType $type, Status $status, array $comments)
+    public function __construct(int $number, string $title, string $body, GitHubUser $author, IssueType $type, Status $status, array $comments)
     {
         $this->number = $number;
         $this->title = $title;
         $this->body = $body;
+        $this->author = $author;
         $this->type = $type;
         $this->status = $status;
         $this->comments = Generics::typeSafeArray($comments, IssueComment::class);
