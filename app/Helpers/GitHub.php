a6e5b8031eb05fea92b20f070f246ed4954e6d48
Caen De Silva <caen@desilva.se>
2024-06-19 21:44:10 +0200

    Refactor to cache API requests

--- app/Helpers/GitHub.php
+++ app/Helpers/GitHub.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Helpers;
 
 use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Cache;
 
 /**
  * General GitHub Facade.
@@ -20,10 +20,15 @@ class GitHub
     /**
      * Create a GitHub API request.
      */
-    public static function request(string $method, string $uri, array $data = []): Response
+    public static function request(string $method, string $uri, array $data = []): array
     {
-        return Http::withToken(env('GITHUB_TOKEN'))
-            ->withHeaders(['Accept' => 'application/vnd.github.v3+json'])
-            ->$method("https://api.github.com/repos/".self::REPOSITORY."/$uri", $data)->throw();
+        $cacheKey = 'api-request-'.sha1(date('Y').$method.$uri.json_encode($data));
+
+        return Cache::rememberForever($cacheKey, function () use ($method, $uri, $data): array {
+            return Http::withToken(env('GITHUB_TOKEN'))
+                ->withHeaders(['Accept' => 'application/vnd.github.v3+json'])
+                ->$method('https://api.github.com/repos/'.self::REPOSITORY."/$uri", $data)
+                ->throw()->json();
+        });
     }
 }
