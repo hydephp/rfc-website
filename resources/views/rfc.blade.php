d08c79f94c9d8d40456185bc8d0b8769ff415946
Caen De Silva <caen@desilva.se>
2024-06-20 20:39:42 +0200

    Begin drafting header

--- resources/views/rfc.blade.php
+++ resources/views/rfc.blade.php
@@ -3,6 +3,16 @@
 
     <main>
         <article>
+            <header>
+                <h1>{{ $title }}</h1>
+                <p class="meta">
+                    Author: {{ $page->author }} <br>
+                    <time datetime="{{ $page->created }}">Created {{ $page->created }}</time>
+                    @if ($page->updated !== $page->created)
+                        &middot; <time datetime="{{ $page->updated }}">Updated {{ $page->updated }}</time>
+                    @endif
+                </p>
+            </header>
             {{ $content }}
         </article>
     </main>
