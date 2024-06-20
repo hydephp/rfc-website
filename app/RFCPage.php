5b80182d6f0c476f6b90bd2b5777d0d1aaa55bf9
Caen De Silva <caen@desilva.se>
2024-06-20 20:11:18 +0200

    Construct using issue object

--- app/RFCPage.php
+++ app/RFCPage.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Types\Issue;
 use Hyde\Pages\MarkdownPage;
 
 class RFCPage extends MarkdownPage
 {
-    //
+    public function __construct(Issue $issue)
+    {
+        parent::__construct("rfc/$issue->number");
+    }
 }
