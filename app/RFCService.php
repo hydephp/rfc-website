2567ecab7ae91cf76a397c87c10b68f64e2981de
Caen De Silva <caen@desilva.se>
2024-06-20 23:14:43 +0200

    Remove extra newline

--- app/RFCService.php
+++ app/RFCService.php
@@ -32,7 +32,6 @@ class RFCService
         $pulls = GitHub::search(['label:RFC', 'is:pr']);
         // $discussions = GitHub::rawSearch('repo%3Ahydephp%2Fdevelop+rfc&type=discussions');
 
-
         // More efficient way to find the user data not included by search data, using unique users to reduce API calls.
         $userCache = $this->getUserCache(Arr::unique([...Arr::pluck($issues['items'], 'user.login'), ...Arr::pluck($pulls['items'], 'user.login')]));
 
