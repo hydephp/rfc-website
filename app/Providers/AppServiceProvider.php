b05a7070c2ecb342080b2e886b9530f74833fb37
Caen De Silva <caen@desilva.se>
2024-06-20 18:41:37 +0200

    Register service as singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\RFCService;
 use App\CallRFCServiceCommand;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->singleton(RFCService::class);
+
         $this->commands([
             CallRFCServiceCommand::class,
         ]);
