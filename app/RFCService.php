31ad9d390290471569e3cbe72b743b957a1d4bfe
Caen De Silva <caen@desilva.se>
2024-06-20 23:11:40 +0200

    Move down user cache

--- app/RFCService.php
+++ app/RFCService.php
@@ -32,8 +32,8 @@ class RFCService
         $pulls = GitHub::search(['label:RFC', 'is:pr']);
         // $discussions = GitHub::rawSearch('repo%3Ahydephp%2Fdevelop+rfc&type=discussions');
 
-        $userCache = [];
         $usersToCache = Arr::unique([...Arr::pluck($issues['items'], 'user.login'), ...Arr::pluck($pulls['items'], 'user.login')]);
+        $userCache = [];
 
         $issues = Arr::map($issues['items'], function (array $issue): Issue {
             return new Issue(
