2a2c4542cd4c89a34975824d1ee2a90b039c0ba8
Caen De Silva <caen@desilva.se>
2024-06-20 19:21:32 +0200

    Update page.blade.php

--- resources/views/vendor/hyde/layouts/page.blade.php
+++ resources/views/vendor/hyde/layouts/page.blade.php
@@ -2,10 +2,8 @@
 @extends('hyde::layouts.app')
 @section('content')
 
-    <main id="content" class="mx-auto max-w-7xl py-16 px-8">
-        <article @class(['mx-auto', config('markdown.prose_classes', 'prose dark:prose-invert'), 'torchlight-enabled' => Features::hasTorchlight()])>
-            {{ $content }}
-        </article>
-    </main>
+    <article>
+        {{ $content }}
+    </article>
 
 @endsection
