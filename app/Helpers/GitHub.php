9a9aedd5ca5eb234e64b7a68cfb1ebb07d83c63f
Caen De Silva <caen@desilva.se>
2024-06-20 20:44:04 +0200

    Revert "Switch to staging repository"
    This reverts commit 7ce6f15809d5e5b865c75e8950ce275cd2615053.

--- app/Helpers/GitHub.php
+++ app/Helpers/GitHub.php
@@ -15,7 +15,7 @@ class GitHub
     /**
      * The repository name on GitHub.
      */
-    public const string REPOSITORY = 'hyde-staging/rfc-develop-test';
+    public const string REPOSITORY = 'hydephp/develop';
 
     /**
      * Create a GitHub API request.
