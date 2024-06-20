3d1ec5e87a117032196adf58cb8292228c5a3527
Caen De Silva <caen@desilva.se>
2024-06-20 18:40:42 +0200

    Create SetupBuildTask.php

---
+++ app/Actions/SetupBuildTask.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Actions;
+
+use Hyde\Framework\Features\BuildTasks\PreBuildTask;
+
+class SetupBuildTask extends PreBuildTask
+{
+    public function handle(): void
+    {
+        //
+    }
+}
