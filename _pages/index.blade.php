4fd09dffaa5cd72013800e97c732f92e1402b3f8
Caen De Silva <caen@desilva.se>
2024-06-20 19:18:12 +0200

    Add basic styling

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 @php($title = "RFCs")
 
-<main class="mx-auto max-w-7xl py-16 px-8">
-    <h1 class="text-center text-3xl font-bold">HydePHP RFCs</h1>
-</main>
+    <header>
+        <h1 class="text-center">HydePHP RFCs</h1>
+    </header>
 
 @endsection
--- resources/views/vendor/hyde/layouts/app.blade.php
+++ resources/views/vendor/hyde/layouts/app.blade.php
@@ -5,10 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $page->title() }}</title>
     @include('hyde::layouts.meta')
+    <style>
+        body {
+            font-family: monospace;
+            font-size: 16px;
+            line-height: 1.5;
+            margin: 0;
+            padding: 0;
+        }
+
+        main {
+            margin: 0 auto;
+            max-width: 800px;
+            padding: 20px;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            font-weight: bold;
+            white-space: pre;
+            font-family: monospace;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        @media print {
+            body {
+                font-size: 10.5pt;
+            }
+
+            a:link, a:visited {
+                color: inherit;
+                text-decoration: none;
+            }
+
+            .no-print {
+                display: none;
+            }
+        }
+    </style>
 </head>
 <body>
-    <section>
+    <main>
         @yield('content')
-    </section>
+    </main>
 </body>
 </html>
