f04c7a9ebee181dfd8a3627945f27c76a7dcce76
Caen De Silva <caen@desilva.se>
2024-06-21 20:43:40 +0200

    Compile the SCSS

--- app/Helpers/SCSS.php
+++ app/Helpers/SCSS.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Helpers;
 
 // See https://github.com/panique/laravel-sass for caching and optimization ideas
+use ScssPhp\ScssPhp\Compiler;
+
 class SCSS
 {
     /**
@@ -12,7 +14,9 @@ class SCSS
      */
     public function compile(string $scss): string
     {
-        //
+        $compiler = new Compiler();
+
+        return $compiler->compileString($scss)->getCss();
     }
 
     /**
