77007761b51aa7fd3656352dfabf841a5e067d3f
Caen De Silva <caen@desilva.se>
2024-06-21 21:52:29 +0200

    Move inline styles to scoped stylesheet section

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -1,43 +1,6 @@
 @extends('hyde::layouts.app')
 @section('content')
 @php($title = "RFCs")
-    <style>
-        header {
-            margin-top: 20px;
-            margin-bottom: 20px;
-        }
-
-        header h1 {
-            font-size: 32px;
-            margin: 0;
-        }
-
-        header p {
-            font-size: 18px;
-            margin: 0;
-        }
-
-        article {
-            color: #333;
-            text-decoration: none;
-            margin-bottom: 20px;
-            margin-left: 40px;
-        }
-
-        article a {
-            text-decoration: none;
-        }
-
-        article h2 {
-            font-size: 20px;
-            margin: 0;
-        }
-
-        article p {
-            margin: 0;
-        }
-    </style>
-
     <header class="text-center">
         <h1>HydePHP RFCs</h1>
         <p role="doc-subtitle">
