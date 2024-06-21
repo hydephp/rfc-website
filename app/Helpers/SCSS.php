9b1f09e20d3f474e6aa71b83613aa77273ea1121
Caen De Silva <caen@desilva.se>
2024-06-21 20:45:48 +0200

    Document code reasoning

--- app/Helpers/SCSS.php
+++ app/Helpers/SCSS.php
@@ -24,6 +24,10 @@ class SCSS
      */
     public function file(string $file): string
     {
+        // In a general production we could return a compiled file,
+        // but since this is a static site, compiled with development
+        // dependencies, we'll just compile it on the fly for simplicity.
+
         $file = basename($file, '.scss');
 
         return $this->compile(file_get_contents(resource_path("scss/$file.scss")));
