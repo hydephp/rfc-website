94a96f4f1ba50b3c6b0b9d6487e7aadcc10cf180
Caen De Silva <caen@desilva.se>
2024-06-21 09:57:47 +0200

    Add helper to compile a file

--- app/Helpers/SCSS.php
+++ app/Helpers/SCSS.php
@@ -13,4 +13,12 @@ class SCSS
     {
         //
     }
+
+    /**
+     * Compile the given SCSS file in the `resources/scss` directory.
+     */
+    public function file(string $file): string
+    {
+        return $this->compile(file_get_contents(resource_path("scss/{$file}.scss")));
+    }
 }
