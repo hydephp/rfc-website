42cb380e474763f9a05a1402e1dfa534a444a3df
Caen De Silva <caen@desilva.se>
2024-06-22 17:14:11 +0200

    Experimental print page numbering

--- resources/views/vendor/hyde/layouts/app.blade.php
+++ resources/views/vendor/hyde/layouts/app.blade.php
@@ -39,5 +39,25 @@
         </small>
     </p>
 </footer>
+    <script type="text/javascript">
+        window.onbeforeprint = function() {
+            addPageNumbers();
+        };
+
+        function addPageNumbers() {
+            console.log("Adding page numbers")
+            var totalPages = Math.ceil(document.body.scrollHeight / 1123);  //842px A4 pageheight for 72dpi, 1123px A4 pageheight for 96dpi,
+            for (var i = 1; i <= totalPages; i++) {
+            var pageNumberDiv = document.createElement("div");
+            var pageNumber = document.createTextNode("Page " + i + " of " + totalPages);
+            pageNumberDiv.style.position = "absolute";
+            pageNumberDiv.style.marginTop = "calc((" + i + " * (297mm - 0.5px)) - 40px)"; //297mm A4 pageheight; 0,5px unknown needed necessary correction value; additional wanted 40px margin from bottom(own element height included)
+            pageNumberDiv.style.height = "16px";
+            pageNumberDiv.appendChild(pageNumber);
+            document.body.insertBefore(pageNumberDiv, document.getElementById("content"));
+            pageNumberDiv.style.left = "calc(100% - (" + pageNumberDiv.offsetWidth + "px + 20px))";
+        }
+    }
+</script>
 </body>
 </html>
