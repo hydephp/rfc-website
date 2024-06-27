843f75fed39999a928e52a53acc78a69354cd1a8
Caen De Silva <caen@desilva.se>
2024-06-27 14:53:10 +0200

    Extract partial

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -26,32 +26,7 @@
 
     <main>
         @foreach(app(\App\RFCService::class)->getItems()->issues() as $issue)
-            <article>
-                <header>
-                    <a href="{{ $issue->link }}">
-                        <h2>{{ $issue->prettyTitle }}</h2>
-                    </a>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th>Author:</th><td>{{ $issue->author->name }}</td>
-                                <th>Status:</th><td>{{ $issue->status }}</td>
-                                <th>Type:</th><td>{{ $issue->type }}</td>
-                                <td><a href="{{ $issue->link }}">View on GitHub</a></td>
-                            </tr>
-                            <tr>
-                                <th>Created:</th><td>{{ $issue->created }}</td>
-                                @if($issue->updated !== $issue->created)
-                                    <th>Updated:</th><td>{{ $issue->updated }}</td>
-                                @endif
-                            </tr>
-                        </tbody>
-                    </table>
-                </header>
-                <p>
-                    {{ $issue->summary() }}
-                </p>
-            </article>
+            @include('rfc-card', ['issue' => $issue])
         @endforeach
     </main>
 @endsection
---
+++ resources/views/rfc-card.blade.php
@@ -0,0 +1,26 @@
+<article>
+    <header>
+        <a href="{{ $issue->link }}">
+            <h2>{{ $issue->prettyTitle }}</h2>
+        </a>
+        <table>
+            <tbody>
+            <tr>
+                <th>Author:</th><td>{{ $issue->author->name }}</td>
+                <th>Status:</th><td>{{ $issue->status }}</td>
+                <th>Type:</th><td>{{ $issue->type }}</td>
+                <td><a href="{{ $issue->link }}">View on GitHub</a></td>
+            </tr>
+            <tr>
+                <th>Created:</th><td>{{ $issue->created }}</td>
+                @if($issue->updated !== $issue->created)
+                    <th>Updated:</th><td>{{ $issue->updated }}</td>
+                @endif
+            </tr>
+            </tbody>
+        </table>
+    </header>
+    <p>
+        {{ $issue->summary() }}
+    </p>
+</article>
