e82998c2a126ba9d0105cfe6f1468f5b66a6a0ab
Caen De Silva <caen@desilva.se>
2024-06-27 14:37:00 +0200

    Draft method to preprocess summary

--- app/Types/Issue.php
+++ app/Types/Issue.php
@@ -90,10 +90,15 @@ readonly class Issue
 
     public function summary(): string
     {
-        $markdown = $this->body->body();
+        $markdown = $this->preprocessSummary($this->body->body());
         $body = (new ConvertsMarkdownToPlainText($markdown))->execute();
 
         return substr($body, 0, 200).'...';
+    }
+
+    protected function preprocessSummary(string $markdown): string
+    {
+        return $markdown;
     }
 
     protected function prettyTitle(): string
