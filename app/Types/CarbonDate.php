8f725fed012e615ced6a0744bb81ac1ea4f48028
Caen De Silva <caen@desilva.se>
2024-06-27 18:07:26 +0200

    Class CarbonDate implements Htmlable

--- app/Types/CarbonDate.php
+++ app/Types/CarbonDate.php
@@ -6,13 +6,20 @@ namespace App\Types;
 
 use Carbon\Carbon;
 use DateTimeImmutable;
+use Illuminate\Support\HtmlString;
+use Illuminate\Contracts\Support\Htmlable;
 
-class CarbonDate
+class CarbonDate implements Htmlable
 {
     protected Carbon $date;
 
     public function __construct(DateTimeImmutable $date)
     {
         $this->date = Carbon::instance($date);
+    }
+
+    public function toHtml(): HtmlString
+    {
+        return new HtmlString(sprintf('%s <span class="text-muted">%s</span>', $this->date->format('F j, Y'), $this->date->format('H:i A')));
     }
 }
