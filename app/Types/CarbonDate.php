7a270873b369c0b5713c2af64c2a3881610ddaf9
Caen De Silva <caen@desilva.se>
2024-06-27 18:06:25 +0200

    Construct Carbon date from DateTime

--- app/Types/CarbonDate.php
+++ app/Types/CarbonDate.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace App\Types;
 
+use Carbon\Carbon;
+use DateTimeImmutable;
+
 class CarbonDate
 {
-    //
+    protected Carbon $date;
+
+    public function __construct(DateTimeImmutable $date)
+    {
+        $this->date = Carbon::instance($date);
+    }
 }
