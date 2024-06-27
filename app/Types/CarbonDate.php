d706755cf1a36393941d9c74277ec3e723f4748c
Caen De Silva <caen@desilva.se>
2024-06-27 18:12:20 +0200

    Class CarbonDate implements Stringable instead of Htmlable

--- app/Types/CarbonDate.php
+++ app/Types/CarbonDate.php
@@ -4,18 +4,23 @@ declare(strict_types=1);
 
 namespace App\Types;
 
+use Stringable;
 use Carbon\Carbon;
 use DateTimeImmutable;
 use Illuminate\Support\HtmlString;
-use Illuminate\Contracts\Support\Htmlable;
 
-class CarbonDate implements Htmlable
+class CarbonDate implements Stringable
 {
     protected Carbon $date;
 
     public function __construct(DateTimeImmutable $date)
     {
         $this->date = Carbon::instance($date);
+    }
+
+    public function __toString(): string
+    {
+        return $this->date->format('Y-m-d H:i:s \U\T\C');
     }
 
     public function toHtml(): HtmlString
