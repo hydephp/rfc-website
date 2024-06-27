cf0bd2babf254b4577d7d78ca630bfd1b6372d03
Caen De Silva <caen@desilva.se>
2024-06-27 14:53:24 +0200

    Fix table indentation

--- resources/views/rfc-card.blade.php
+++ resources/views/rfc-card.blade.php
@@ -5,18 +5,18 @@
         </a>
         <table>
             <tbody>
-            <tr>
-                <th>Author:</th><td>{{ $issue->author->name }}</td>
-                <th>Status:</th><td>{{ $issue->status }}</td>
-                <th>Type:</th><td>{{ $issue->type }}</td>
-                <td><a href="{{ $issue->link }}">View on GitHub</a></td>
-            </tr>
-            <tr>
-                <th>Created:</th><td>{{ $issue->created }}</td>
-                @if($issue->updated !== $issue->created)
-                    <th>Updated:</th><td>{{ $issue->updated }}</td>
-                @endif
-            </tr>
+                <tr>
+                    <th>Author:</th><td>{{ $issue->author->name }}</td>
+                    <th>Status:</th><td>{{ $issue->status }}</td>
+                    <th>Type:</th><td>{{ $issue->type }}</td>
+                    <td><a href="{{ $issue->link }}">View on GitHub</a></td>
+                </tr>
+                <tr>
+                    <th>Created:</th><td>{{ $issue->created }}</td>
+                    @if($issue->updated !== $issue->created)
+                        <th>Updated:</th><td>{{ $issue->updated }}</td>
+                    @endif
+                </tr>
             </tbody>
         </table>
     </header>
